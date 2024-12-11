e5197809487b83d0cbb43a3b1199b374fa0099b7
liuhy <liuhongyu@apache.org>
2024-12-11 19:31:51 +0800

    v

--- serviceb/src/main/java/com/example/serviceb/controller/ServiceBRestController.java
+++ serviceb/src/main/java/com/example/serviceb/controller/ServiceBRestController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ServiceBRestController {
 
@@ -19,11 +22,31 @@ public class ServiceBRestController {
 
 	@GetMapping("helloEureka")
 	public String helloWorld() {
+		List<String> list = discoveryClient.getServices();
+		for (String s : list) {
+			List<ServiceInstance> serviceInstances = discoveryClient.getInstances(s);
+			System.out.println("Service: " + s);
+		}
 		ServiceInstance serviceInstance = discoveryClient.getInstances("servicea").get(0);
 		String serviceAResponse = restClient.get()
 				.uri(serviceInstance.getUri() + "/helloWorld")
 				.retrieve()
 				.body(String.class);
 		return serviceAResponse;
+	}
+	@GetMapping("services")
+	public List<ServiceInstance> services() {
+		List<String> list = discoveryClient.getServices();
+		List<ServiceInstance> serviceInstanceAll = new ArrayList<>();
+		for (String s : list) {
+			List<ServiceInstance> serviceInstances = discoveryClient.getInstances(s);
+			serviceInstanceAll.addAll(serviceInstances);
+		}
+//		ServiceInstance serviceInstance = discoveryClient.getInstances("servicea").get(0);
+//		String serviceAResponse = restClient.get()
+//				.uri(serviceInstance.getUri() + "/helloWorld")
+//				.retrieve()
+//				.body(String.class);
+		return serviceInstanceAll;
 	}
 }
